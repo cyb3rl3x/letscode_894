8f063c88aa58b9b2711e690aa347be2e0272bc11
pro-cognitron <pro.cognitron@gmail.com>
2022-11-14 16:50:20 -0300

    1.8

--- modulo/calculadora.py
+++ modulo/calculadora.py
@@ -1,22 +1,5 @@
 def soma(x, y):
     return x + y
 
-def subtracao(x, y):
-    return x-y
-
-def multiplicacao(x, y):
-    return x*y
-    
-def divisao(x, y):
-    return x/y
-    
-def incremento(x):
-    return x+1
-    
-def decremento(x):
-    return x-1
-
-def potenciacao(x, y):
-    return x^y
     
     
